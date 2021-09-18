dfeb8ed3241d4854672d314f05aabc9984713042
김종환 <dondon9513@gmail.com>
2021-09-18 13:17:24 +0900

    apply event delegation to shopping app

--- DOM/main.js
+++ DOM/main.js
@@ -19,33 +19,21 @@ function onAdd() {
   input.value = "";
   input.focus();
 }
-
+let id = 0; // UUID와같은 API쓰는게 좋다
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
-
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const name = document.createElement("span");
-  name.setAttribute("class", "item__name");
-  name.innerText = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item__delete");
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  deleteBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item__divider");
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+        <div class="item">
+          <span class="item__name">${text}</span>
+          <button class="item__delete">
+            <i class="fas fa-trash-alt" data-id=${id}></i>
+          </button>
+        </div>
+        <div class="item__divider"></div>
+  `;
+  id++;
 
   return itemRow;
 }
@@ -57,5 +45,13 @@ addBtn.addEventListener("click", () => {
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
